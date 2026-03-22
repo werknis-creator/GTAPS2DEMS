cfcb22778a01dc2c20ee4738788e399490e03bd9
werknis-creator <werknis@gmail.com>
2026-03-22 22:37:38 +0100

    Update gta.cpp

--- gta.cpp
+++ gta.cpp
@@ -4,11 +4,13 @@
 int main(int argc, char *argv[]) {
     GSGLOBAL *gsGlobal = gsKit_init_global();
     
-    // Zmieniono z dmaKit_init_all na dmaKit_init
-    dmaKit_init(); 
+    // Ustawiamy dmaKit z domyslnymi parametrami (D_CTRL register settings)
+    // D_CTRL: RELE=0, MFD=0, STS=0, STD=0, RCYC=0, RELEASE_TIME=0
+    dmaKit_init(0, 0, 0, 0, 0, 0); 
+    
     gsKit_init_screen(gsGlobal);
 
-    // Reczne ustawienie koloru niebieskiego
+    // Standardowy kolor niebieski
     u64 Blue = GS_SETREG_RGBA(0x00, 0x00, 0xFF, 0x80);
 
     while(1) {
