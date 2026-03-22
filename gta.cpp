eccf17196571c4cfb98d9c6ac4c82e364b200bf5
werknis-creator <werknis@gmail.com>
2026-03-22 22:26:39 +0100

    Update gta.cpp

--- gta.cpp
+++ gta.cpp
@@ -1,17 +1,19 @@
 #include <gsKit.h>
 #include <dmaKit.h>
 
-int main() {
+int main(int argc, char *argv[]) {
     GSGLOBAL *gsGlobal = gsKit_init_global();
+    
     dmaKit_init_all();
     gsKit_init_screen(gsGlobal);
 
     while(1) {
-        // Czyścimy ekran na niebiesko
-        gsKit_clear(gsGlobal, GS_SETREG_RGBAQ(0, 0, 255, 0, 0));
+        // Czyścimy ekran na kolor niebieski (RGBA: 0, 0, 255, 128)
+        gsKit_clear(gsGlobal, GS_SETREG_RGBAQ(0x00, 0x00, 0xFF, 0x80, 0x00));
         
         gsKit_queue_exec(gsGlobal);
         gsKit_sync_flip(gsGlobal);
     }
+
     return 0;
 }
