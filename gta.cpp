d400b1366bf7b88b6565e7d9467a7c15f7455950
werknis-creator <werknis@gmail.com>
2026-03-22 22:21:56 +0100

    gta.cpp

--- gta.cpp
+++ gta.cpp
@@ -1,19 +1,24 @@
 #include <gsKit.h>
 #include <dmaKit.h>
-#include <malloc.h>
 
 int main(int argc, char *argv[]) {
     GSGLOBAL *gsGlobal = gsKit_init_global();
     dmaKit_init_all();
     gsKit_init_screen(gsGlobal);
 
-    u64 Color = GS_SETREG_RGBAQ(255, 0, 0, 0x80, 0x00);
+    // Definiujemy czerwony kolor
+    u64 Red = GS_SETREG_RGBAQ(255, 0, 0, 0x80, 0x00);
 
     while(1) {
-        gsKit_clear(gsGlobal, GS_SETREG_RGBAQ(0x00, 0x40, 0x00, 0x00, 0x00));
-        gsKit_prim_sprite(gsGlobal, 200.0f, 200.0f, 300.0f, 300.0f, 1, Color);
+        // Tło: Ciemna zieleń (jak trawa w starym GTA)
+        gsKit_clear(gsGlobal, GS_SETREG_RGBAQ(0, 50, 0, 0, 0));
+
+        // Nasze "auto": Czerwony kwadrat na środku
+        gsKit_prim_sprite(gsGlobal, 250.0f, 200.0f, 350.0f, 300.0f, 1, Red);
+
         gsKit_queue_exec(gsGlobal);
         gsKit_sync_flip(gsGlobal);
     }
+
     return 0;
 }
